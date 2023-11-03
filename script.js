7e07c51360b762d4f8656e34d486113416db1de8
hungwnguyen <hanoihung123321@gmail.com>
2023-11-03 22:13:37 +0700

    fix to be, past tense verb not found

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ function searchWord() {
       let inputString = data.enEnData.best.details;
       console.log(inputString);
       let startString = 'class=\"content\" style=\"padding-bottom: 10px\">\n    \n    <div class=\"bg-grey bold font-large m-top20\"><span>';
-      let endString = '</span>';
+      let endString = '<';
   
-      let startIndex = inputString.indexOf(startString) + startString.length;
-      let endIndex = inputString.indexOf(endString, startIndex);
-  
-      let outputString = inputString.substring(startIndex, endIndex);
+      let outputString = extractSubstring(inputString, startString, endString);
+      if (outputString.substring(outputString.length - 2) == '">'){
+        startString = '<div id=\"content_selectable\" class=\"content\" style=\"padding-bottom: 10px\">\n    \n    <div class=\"\">';
+        outputString = extractSubstring(inputString, startString, endString);
+      }
       resultDiv.innerHTML = outputString;
     })
     .catch(function(error) {
@@ -29,3 +30,15 @@ function searchWord() {
       resultDiv.innerHTML = "Đã xảy ra lỗi khi gọi API.";
     });
   }
+
+  function extractSubstring(inputString, startString, endString) {
+    let startIndex = inputString.indexOf(startString) + startString.length;
+    let endIndex = inputString.indexOf(endString, startIndex);
+  
+    if (startIndex >= 0 && endIndex >= 0 && endIndex > startIndex) {
+      return inputString.substring(startIndex, endIndex);
+    } else {
+      return "";
+    }
+  }
+  
