e7c08a4f8f3aa30559e9c6506b44976888473752
hungwnguyen <hanoihung123321@gmail.com>
2023-11-07 20:59:19 +0700

    update type of word

---
+++ JS/PassSimple.js
@@ -0,0 +1,32 @@
+// Hàm tính giá trị thích nghi (fitness)
+function calculateFitness() {
+  // Thực hiện tính toán giá trị thích nghi ở đây
+}
+
+// Hàm chọn (Selection)
+function selection() {
+  // Thực hiện quá trình chọn cá thể ở đây
+}
+
+// Hàm lai ghép (Crossover)
+function crossover() {
+  // Thực hiện quá trình lai ghép ở đây
+}
+
+// Hàm đột biến (Mutation)
+function mutation() {
+  // Thực hiện quá trình đột biến ở đây
+}
+
+// Hàm chạy giải thuật di truyền
+function sga_passSimple(adam, eva) {
+  // Khởi tạo các tham số và tiêu chí dừng ở đây
+  console.log(adam + " & " + eva);
+//   while (fitnessValue !== terminationCriteria) {
+//     calculateFitness();
+//     selection();
+//     crossover();
+//     mutation();
+//     calculateFitness();
+//   }
+}
--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,66 @@
-function searchWord() {
-  var word = document.getElementById("wordInput").value;
+async function initializePopulation() {
   var resultDiv = document.getElementById("result");
-
-  if (word.trim() === "") {
-    resultDiv.innerHTML = "Vui lòng nhập từ tiếng Anh.";
+  var inputSentence = document.getElementById("sentenceInput").value;
+  let words = inputSentence.split(/\s+/);
+  if (words.length < 2 || words.length > 6) {
+    animateText("Vui lòng nhập lại");
     return;
   }
+  let shuffledWords = shuffleArray(words); // Hoán vị ngẫu nhiên các từ
+  let individual = shuffledWords.join(' '); // Tạo câu mới từ các từ đã hoán vị
 
-  var apiUrl = "https://dict.laban.vn/ajax/widget-search?type=1&query=" + encodeURIComponent(word) + "&vi=0";
-  fetch(apiUrl)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
+  for (let i = 0; i < words.length; i++) {
+    const element = words[i];
+    const test = await searchWord(element);
+
+    if (test == '') {
+      animateText("Vui lòng nhập đúng từ tiếng Anh.");
+      break; // Dừng vòng lặp nếu gặp lỗi
+    }
+    if (test.substring(0, 14) == ' past tense of') {
+      sga_passSimple(inputSentence, individual);
+      break;
+    }
+  }
+}
+
+// Hàm hoán vị mảng ngẫu nhiên
+function shuffleArray(array) {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
+async function searchWord(word) {
+  if (word.trim() === "") {
+    return '';
+  }
+
+  try {
+    const apiUrl = "https://dict.laban.vn/ajax/widget-search?type=1&query=" + encodeURIComponent(word) + "&vi=0";
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
     let inputString = data.enEnData.best.details;
     let startString = '<div class=\"bg-grey bold font-large m-top20\"><span>';
     let endString = '<';
 
     let outputString = extractSubstring(inputString, startString, endString);
-    if (outputString.length > 1){
-      if (outputString.substring(outputString.length - 2) == '">'){
-        startString = '<div id=\"content_selectable\" class=\"content\" style=\"padding-bottom: 10px\">\n    \n    <div class=\"\">';
-        outputString = extractSubstring(inputString, startString, endString);
-      }
+    
+    if (outputString == '') {
+      startString = '<div id=\"content_selectable\" class=\"content\" style=\"padding-bottom: 10px\">\n    \n    <div class=\"\">';
+      outputString = extractSubstring(inputString, startString, endString);
     }
-    resultDiv.innerHTML = outputString;
-  })
-  .catch(function(error) {
+    return outputString;
+  } catch (error) {
     console.log(error);
-    resultDiv.innerHTML = "Đã xảy ra lỗi khi gọi API.";
-  });
+    return '';
+  }
 }
+
 
 function extractSubstring(inputString, startString, endString) {
   let result = '';
@@ -58,6 +88,26 @@ function extractSubstring(inputString, startString, endString) {
 
   return result;
 }
-  
-  
-  
+
+function animateText(text, minTimeout = 0, maxTimeout = 1, underscoreTimeout = 66) {
+  let currentIndex = 0;
+  const element = document.getElementById("result"); // ID của phần tử hiển thị kết quả
+  element.innerHTML = '';
+  const typeCharacter = () => {
+    if (currentIndex < text.length) {
+      element.innerHTML = element.innerHTML.slice(0, -1) + text[currentIndex++] + '<span class="long-pipe">|</span>';
+      setTimeout(() => {
+        if (currentIndex < text.length) {
+          element.textContent = element.textContent.slice(0, -1) + '|';
+        }
+        const timeout = Math.floor(Math.random() * (maxTimeout - minTimeout + 1)) + minTimeout;
+        setTimeout(typeCharacter, timeout);
+      }, underscoreTimeout);
+    } else {
+      element.textContent = element.textContent.slice(0, -1);
+    }
+  };
+
+  typeCharacter();
+}
+ 
